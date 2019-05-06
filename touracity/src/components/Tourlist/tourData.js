17faf6a2aa50ddb6e4583f77cc1590b05a3fd0b1
Alfredo <puertorocat@gmail.com>
2019-05-06 04:38:06 -0400

    added city information and styling

--- touracity/src/components/Tourlist/tourData.js
+++ touracity/src/components/Tourlist/tourData.js
@@ -3,9 +3,9 @@ export const tourData = [
       id: 1,
       city: "new york",
       img: "./images/newyork.jpeg",
-      name: "new york bridge tour",
+      name: "new york tour",
       info:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+        "Simply put, the Big Apple, has everything a tourist is looking for. From spectacular Broadway shows to iconic shops, museums and art. It's no surprise the city that never sleeps is top in North America when it comes to international travel."
     },
     {
       id: 2,
@@ -13,7 +13,7 @@ export const tourData = [
       img: "./images/paris.jpeg",
       name: "paris lights tour",
       info:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+        "Home to internationally renowned art, cuisine, music and architecture, you can spend as long as you like in Paris and never become bored. In fact, more than 18 million international overnight visitors went there last year alone."
     },
     {
       id: 3,
@@ -21,14 +21,46 @@ export const tourData = [
       img: "./images/london.jpeg",
       name: "london royal palace tour",
       info:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+        "London might just have everything you are looking for in a major city. Its rich historical background paired with unique landmarks such as the Houses of Parliament and Big Ben, the London Eye, and Buckingham Palace are just a few of the attractions London has to offer."
     },
     {
       id: 4,
       city: "tokyo",
       img: "./images/tokyo.jpeg",
-      name: "tokyo sushi tour",
+      name: "tokyo tour",
       info:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
-    }
+        "The city itself can appear to first-time visitors as if it was pulled out of a sci-fi movie, with its modern architecture and bustling nightlife. A blend of the new and the old, Tokyo is an attractive destination for adventurous travelers - and you can even find historic national parks and ancient temples can be explored while you're there."
+    },
+    {
+        id: 5,
+        city: "berlin",
+        img: "./images/Berlin.jpeg",
+        name: "berlin tour",
+        info:
+          "Berlin is the capital of reunited Germany.Berlin is famous for Berlin wall, museums, architectural buildings,opera house, clubs and theaters.Berlin people are known for their sense of humour and their quickness to give a witty answer.Berlin has a friendly and lovely atmosphere"
+      },
+      {
+        id: 6,
+        city: "dubai",
+        img: "./images/Dubai.jpeg",
+        name: "dubai tour",
+        info:
+          "And if you are looking to take a trip for pleasure rather than business, there's plenty for you to see. In the Burj Khalifa, Dubai is home to the is the tallest structure in the world, and it also packs in plenty of entertainment and shopping. Fun fact: roughly 85% of Dubai's population are expats."
+      },
+      {
+        id: 7,
+        city: "seoul",
+        img: "./images/Seoul, South Korea.jpeg",
+        name: "seoul city tour",
+        info:
+          "Home to more than 10 million people, Seoul features one of the world’s most advanced and efficient transportation infrastructures. Also, their restaurant and food industry is widely-regarded as some of the most efficiently run in the world (helpful hint: tipping isn’t typically required).!"
+      },
+      {
+        id: 8,
+        city: "singapore",
+        img: "./images/Singapore.jpeg",
+        name: "singapore tour",
+        info:
+          "Over 12 million tourists visited in both 2015 and 2016. Its remarkable local cuisine, beautiful gardens and wondrous temples make it a surefire win as a vacation destination. Given its importance as a business hub too, it's no surprise that it's the sixth most popular destination in the world for travel."
+      },
   ];

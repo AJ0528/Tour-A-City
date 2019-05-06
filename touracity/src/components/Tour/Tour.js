a2a0941fcdb22b94b08911ec7c47220a35f0aa5d
Alfredo <puertorocat@gmail.com>
2019-05-06 03:50:22 -0400

    tour data, tour card styling, and logic

--- touracity/src/components/Tour/Tour.js
+++ touracity/src/components/Tour/Tour.js
@@ -3,29 +3,26 @@ import "./Tour.scss";
 
 export default class Tour extends Component {
   render() {
-    return <article className="tour">
+    return (
+        <article className="tour">
     <div className="img-container">
-    <img width="200"src="https://images.pexels.com/photos/2131801/pexels-photo-2131801.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-    alt=""
-     />
+    <img src="https://images.pexels.com/photos/2113558/pexels-photo-2113558.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""  />
      <span className="close-btn">
      <i className="fas fa-window-close"/>
-     
-     </span>
-    
-    
-    </div>
+        </span>
+     </div>
     <div className="tour-info">
     <h3>City</h3>
     <h4>Name</h4>
-    <h5>Info {" "}
+    <h5>info {" "}
         <span>
             <i className="fas fa-caret-square-down" />
         </span>
         </h5>
-        <p>lorem </p>
-    </div>
-
+        <p>Lorem Ipsum is simply dummy text of the printing</p>
+       </div> 
     </article>
+  
+    );
   }
 }
--- touracity/src/components/Tourlist/index.js
+++ touracity/src/components/Tourlist/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import "./Tourlist.scss";
 import Tour from "../Tour/Tour";
+import {tourData} from "./tourData";
 
 export default class Tourlist extends Component {
+    state={
+        tours: tourData
+    };
   render() {
+      const {tours} = this.state;
+   
     return (
-     <section className="tour-list">
-         <Tour />
+     <section className="tourlist">
+    {tours.map(tour =>{
+        return <Tour key={tour.id} tour={tour} />
+    })}
      </section>
-    )
+    );
   }
 }

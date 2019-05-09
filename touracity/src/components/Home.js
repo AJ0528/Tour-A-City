7439ae51e41072a8ff1060c72364159cf9ea0e51
Alfredo <puertorocat@gmail.com>
2019-05-09 10:50:50 -0400

    new changes

--- touracity/src/components/Home.js
+++ touracity/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <p>Aliquip dolor non amet pariatur. Commodo magna minim aute velit laboris culpa aliqua elit nulla voluptate id. In reprehenderit ipsum minim exercitation. Excepteur sit labore occaecat labore occaecat sint sunt do voluptate deserunt do et ullamco. Anim ea irure sit consectetur dolore cillum aliquip reprehenderit elit adipisicing. Occaecat amet reprehenderit proident commodo quis enim.</p>
+            
         </div>
     );
 }
